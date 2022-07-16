3b5f29d18b5e1c901b461e6b253f662f9d217711
Ezra-Kipchumba <ezra.kipchumba@student.moringaschool.com>
2022-07-16 20:35:15 +0300

    modify js and html by adding content

--- dairy.js
+++ dairy.js
@@ -24,37 +24,64 @@ const ofShed4 = (cows=22, milk= 26) => {
 }
 console.log (ofShed4());
 
+ // This is the amount of milk produced in 4 sheds 
+const ofSheds = {
+    ofShed1 : 510,
+    ofShed2 : 308,
+    ofShed3 : 486,
+    ofShed4 : 572,
+  };
+  document.getElementById("daily").innerHTML = ofSheds.ofShed1 +  ofSheds.ofShed2 + ofSheds.ofShed3 + ofSheds.ofShed4;
 
-// const ofSheds = {
-//     ofShed1 : 510,
-//     ofShed2 : 308,
-//     ofShed3 : 486,
-//     ofShed4 : 572,
-//   };
-//   document.getElementById("daily").innerHTML = ofSheds.ofShed1 +  ofSheds.ofShed2 + ofSheds.ofShed3 + ofSheds.ofShed4;
+// // Total amount of milk produced in all sheds 
+
+// function totalProducts(Shed1= 510, Shed2=308, Shed3=486, Shed4=572)
+//  {
+//     return Shed1 + Shed2 + Shed3 + Shed4;
+
+// }
+
+// console.log(totalProducts());
+
+// // // Amount from milksales in  a week
+
+// const ofShedsweek = (totalProducts=1876, days= 7) => {
+//     return 1876 * 7;
+
+// }
+// console.log (ofShedsweek());
 
 
+// // Litres produced on a leap year 
 
-// Total amount of milk produced in all sheds 
+// const ofShedyear = (totalProducts=1876, days= 366) => {
+//     return 1876 * 366;
 
-function totalProducts(Shed1= 510, Shed2=308, Shed3=486, Shed4=572)
- {
-    return Shed1 + Shed2 + Shed3 + Shed4;
+// }
+// console.log (ofShedyear());
 
-}
+// // const oneWeek = (ofSheds=13132, buyingPrice=45) =>{
+// //     return ofSheds * 45;
+// // }
+// // console.log (oneWeek())
 
-console.log(totalProducts());
+// // const incomeMonth = (oneWeek=590940, )
 
-// // Amount from milksales in  a week
+// // How much you will make per month
 
-const ofSheds = (totalProducts=1876, days= 7) => {
-    return 1876 * 7;
-
-}
-console.log (ofSheds());
-
-const oneWeek = (ofSheds=13132, buyingPrice=45) =>{
-    return ofSheds * 45;
-}
-console.log (oneWeek())
+// const incomeMonthly = {
+//     January : 2617020,
+//     February : 2448180,
+//     March : 2617020,
+//     April : 2532600,
+//     May : 2617020,
+//     June : 2532600,
+//     July : 2617020,
+//     August : 2617020,
+//     September : 2532600,
+//     October : 2617020,
+//     November : 2532600,
+//     December : 2617020,
+// };
+// document.getElementById("monthly").innerHTML = January , February , March , April , May , June ,July , August ,September , October ,November , December ;
 
